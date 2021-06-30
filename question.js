ee4ebf74c6211533a7d8edd28f7ae966146a1712
Kahbei <mengus.gary@gmail.com>
2021-06-30 10:38:28 +0200

    Modify index and add js files

---
+++ question.js
@@ -0,0 +1,57 @@
+export const questions = [
+    {
+        question: "Les Américains n’ont jamais marché sur la Lune",
+        type: "boolean",
+        reponse: 1,
+        explication:
+            "Les rumeurs sur le programme Apollo sont une théorie du complot selon laquelle les vaisseaux du programme Apollo ne se seraient jamais posés sur la Lune et qu’il s’agirait en fait d’une mise en scène réalisée sur Terre par la NASA. Les preuves photographiques et les films auraient été fabriqués de toutes pièces, y compris les prélèvements de roches sur la Lune. Les adeptes de la théorie du complot invoquent le fait que le drapeau flotte au vent alors qu’il n’y a pas d’air, que les réticules de cadrage apparaissent curieusement masqués, ou encore que les astronautes et les objets restent étonnement bien visibles dans les ombres… Des théories balayées par des preuves, elles, bien argumentées.",
+        proposition: [
+            {
+                id: 0,
+                text: true,
+            },
+            {
+                id: 1,
+                text: false,
+            },
+        ],
+        image: "https://trendly.fr/wp-content/uploads/sites/3/2016/08/theorie-complot-homme-marcher-lune.jpg",
+    },
+    {
+        question: "Diana, la princesse de Galles, a été assassinée ?",
+        type: "boolean",
+        reponse: 1,
+        explication:
+            "Malgré une enquête officielle qui n’a trouvé aucune preuve d’un complot par le MI6 ou toute autre entité pour assassiner la princesse Diana et Dodi Fayed en 1997, une fiévreuse spéculation continue autour de ce drame. La théorie veut que des “éléments” dans les services secrets britanniques aient décidé que la relation entre Diana et Fayed était une menace pour la monarchie et, par conséquent, pour l’Etat britannique. Un complot a été établi dans lequel une Fiat Uno blanche transportant des agents a été envoyée pour aveugler et désorienter le chauffeur Henri Paul, alors qu’il roulait dans le tunnel situé au niveau du pont de l’Alma. Plus tard, l’échantillon du sang de Henri Paul aurait été échangé avec celui de quelqu’un ayant bu beaucoup d’alcool. Le problème avec cette théorie ? Pas la moindre preuve existe pour la soutenir.",
+        proposition: [
+            {
+                id: 0,
+                text: true,
+            },
+            {
+                id: 1,
+                text: false,
+            },
+        ],
+        image: "https://trendly.fr/wp-content/uploads/sites/3/2016/08/theorie-complot-princesse-diana.jpg",
+    },
+    {
+        question:
+            "Quels arguments pourraient contredire cette idée : La Terre est plate car quand je regarde l'horizon je ne vois pas la courbure de la terre, plusieurs choix peuvent être attendu",
+        type: "multiple",
+        reponse: 2,
+        explication:
+            "Cet argument se base sur le fait qu'on ne peut très rarement voir la courbure de la terre cela est dû à la nôtre taille par rapport à la Terre. Pour le démontrer vous pouvez faire l'expérience suivante : Prenez un globe est placez y une caméra a sa surface vous constaterez la courbure de la terre s'aplanie. A l'opposé plus on s'élève de la surface plus on peut voir la courbure de la terre. On ne peut pas utiliser l'argument B car il pourrait contre argumenter avec un défaut de l'objectif de l'appareil (Pour plus de débunkage sur ce sujet voir : 🌎 La Terre est ronde. C'est ton encéphalogramme qui est plat. DEFAKATOR : https://youtu.be/g-2p2Ahrfts)",
+        proposition: [
+            {
+                id: 0,
+                text: true,
+            },
+            {
+                id: 1,
+                text: false,
+            },
+        ],
+        image: "https://trendly.fr/wp-content/uploads/sites/3/2016/08/theorie-complot-princesse-diana.jpg",
+    },
+];
