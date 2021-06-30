7f04ab42a98ff5659aa6a2413fd69c787d7e4c8f
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-06-30 17:11:56 +0200

    Debut modif pour ajouter des images aux boutons

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,3 +1,6 @@
+imgOrtext(false)
+
+
 /**
  * fonction pour afficher la correction une fois les réponses entrées
  */
@@ -24,4 +27,15 @@ function wrongAnswer(idButton) {
     var bouton = document.getElementById(idButton);
     bouton.style.backgroundColor = "#910f1c";
     showAnswer()
+}
+
+function imgOrtext(isImage) {
+    if (isImage === true) {
+        var listImg = document.getElementsByClassName('btnReponse');
+        console.log(listImg);
+        for (let img of listImg) {
+            img.classList.remove("btnReponse")
+            img.classList.add("btnReponse.image")
+        }
+    }
 }
