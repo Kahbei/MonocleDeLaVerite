f1213ad8491dd34a1cebc4e739f215426d6b0e0a
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-07-01 12:01:11 +0200

    fix de problems car je suis magicien

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,5 +1,4 @@
-imgOrtext(true)
-
+imgOrtext(false)
 
 /**
  * fonction pour afficher la correction une fois les réponses entrées
@@ -29,13 +28,32 @@ function wrongAnswer(idButton) {
     showAnswer()
 }
 
+/**
+ * Si le paramètre donné est true alors on change la taille des boutons pour
+ * qu'ils puissent accueillir une image.
+ * Sinon on cache simplement l'image de "présentation" sous la question.
+ * @param {bool} isImage 
+ */
 function imgOrtext(isImage) {
     if (isImage === true) {
-        var listImg = document.getElementsByClassName('btnReponse');
-        console.log(listImg);
+        //redéfinir la taille des boutons pour accueillir les images
+        var listBtn = document.getElementsByClassName('btn');
+        for (let btn of listBtn) {
+            btn.classList.add("btnImage")
+        }
+        //enlever l'image de "présentation"
+        var imgPres = document.getElementById('imageComplotDiv');
+        imgPres.style.display = "none";
+        //et enlever le texte dans les boutons
+        var listText = document.getElementsByClassName('preponses');
+        for (let text of listText) {
+            text.style.display = "none";
+        }
+    } else {
+        //mettre les images en invisible
+        var listImg = document.getElementsByClassName('ireponses');
         for (let img of listImg) {
-            img.classList.remove("btnReponse")
-            img.classList.add("btnReponse.image")
+            img.style.display = "none";
         }
     }
 }
