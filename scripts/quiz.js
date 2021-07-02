bf3419feea5c56eb2908d3d8991543f6b6d885b5
Kahbei <mengus.gary@gmail.com>
2021-07-02 10:45:21 +0200

    Add image for resultat image

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -10,18 +10,37 @@ const display = {
 
     endQuiz: function () {
         const bareme = [
-            "Tu n'as pas d'esprit critique, pense à le travailler et ne va pas sur facebook sinon tu deviendras l'aigle.",
-            "Tu as les prémices de l'esprit critique , mais tu as encore beaucoup de travail devant toi jeune padawan.",
-            "Tu t'ouvres peu à peu au monde avec jugeote. Continue comme ça !",
-            "Tu as un bon esprit critique de base mais tu es encore loin du compte.",
-            "Tu as un très bon esprit critique, n'arrête jamais de réfléchir.",
-            "Comment peut-on être aussi omniscient ? Si tous le monde était comme toi, le monde serait bien plus calme et agréable à y vivre. Il n'y aurait plus de fausse informations car plus personne pour y croire !",
+            {
+                image: "https://i.kym-cdn.com/entries/icons/original/000/008/305/its-a-conspiracy.jpg",
+                text: "Tu n'as pas d'esprit critique, pense à le travailler et ne va pas sur facebook sinon tu deviendras l'aigle.",
+            },
+            {
+                image: "https://compote.slate.com/images/7bebd458-dfe9-4c35-beee-da10caaaa99b.jpg",
+                text: "Tu as les prémices de l'esprit critique , mais tu as encore beaucoup de travail devant toi jeune padawan.",
+            },
+            {
+                image: "https://i.kym-cdn.com/entries/icons/original/000/022/524/tumblr_o16n2kBlpX1ta3qyvo1_1280.jpg",
+                text: "Tu t'ouvres peu à peu au monde avec jugeote. Continue comme ça !",
+            },
+            {
+                image: "https://i.imgur.com/CP1f2X8.jpg",
+                text: "Tu as un bon esprit critique de base mais tu es encore loin du compte.",
+            },
+            {
+                image: "https://i.imgur.com/euRu9De.gif",
+                text: "Tu as un très bon esprit critique, n'arrête jamais de réfléchir.",
+            },
+            {
+                image: "https://i.pinimg.com/originals/ae/20/0d/ae200d97b9617690050fe2b54b769dd3.gif",
+                text: "Comment peut-on être aussi omniscient ? Si tous le monde était comme toi, le monde serait bien plus calme et agréable à y vivre. Il n'y aurait plus de fausse informations car plus personne pour y croire !",
+            },
         ];
         const endQuizHTML = `
             <div class="resultat">
                 <h1>Quiz terminé !</h1>
                 <h3> Votre score est de : ${quiz.score} / ${quiz.maxQuestion}</h3>
-                <p>${bareme[quiz.score]}</p>
+                <img src="${bareme[quiz.score].image}" alt="illustration de la note">
+                <p>${bareme[quiz.score].text}</p>
             </div>
         `;
         this.elementShown("quiz", endQuizHTML);
